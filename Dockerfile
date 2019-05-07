814b3defcc0c4f9d05ca381ea7b6ea4bca15a15b
VKatore <vkatore@slb.com>
2019-05-07 19:01:16 +0530

    added configmap integration

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM openjdk:8-jdk-alpine
-COPY target /opt/dqa-boot-service
-WORKDIR /opt/dqa-boot-service/target
-EXPOSE 9090
+RUN mkdir -p /spring-boot-service
+ADD target/* /spring-boot-service/
+WORKDIR /spring-boot-service
+RUN ls -l
+EXPOSE 8080
+RUN pwd
 ENTRYPOINT ["java","-jar","gs-rest-service-0.1.0.jar"]
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,19 +1,36 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+
 @RestController
 public class GreetingController {
+
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    @Value("${name}")
+    private String name;
+
+    @Value("${country}")
+    private String country;
+
+
+
+    @RequestMapping("/greeting/{pathVariable}")
+    public Greeting greeting(@PathVariable("pathVariable") String pathVariable) {
+        System.out.println(name);
+        String value = "default value";
+        if(pathVariable.equalsIgnoreCase("name")) value = name;
+        else if (pathVariable.equalsIgnoreCase("country")) value = country;
+        return new Greeting(counter.incrementAndGet(),String.format(template, value));
     }
 }
